84478073be49036b801e2f6b2811938916d04020
RTN <RTN@DESKTOP-1AD0JTR>
2022-06-23 15:31:02 +0700

    Add project files.

---
+++ SkillsBuild/SkillsBuild.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+    cout << "Hello World!\n";
+    int a = 10;
+    int b = 20;
+    int c = a + b;
+    cout << c <<"\n";
+    cout << a * c;
+    int num[5] = {1,2,3,4,5};
+    for (int i = 0; i < 5; i++) {
+        cout << num[i]<< "";
+    }
+}
+
